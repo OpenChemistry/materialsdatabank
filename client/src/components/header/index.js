38e8cb06e2365f9989a9139d5fdf78170bbae7bf
Chris Harris <chris.harris@kitware.com>
2017-09-26 16:55:55 -0400

    Add search bar and title

--- client/src/components/header/index.js
+++ client/src/components/header/index.js
@@ -6,13 +6,31 @@ import logo from './OpenChemistry_Logo.svg';
 
 
 const style = {
-    backgroundColor: '#FAFAFA',
+    backgroundColor: '#FAFAFA'
+}
+
+const titleStyle = {
+  margin: 0,
+  paddingTop: '8px',
+  paddingLeft: '10px',
+
+  lineHeight: 'normal',
+  color: '#424242',
 }
 
 export default class Header extends Component {
   render = () => {
   return (
-    <AppBar style={style} iconElementLeft={<img className='mdb-logo' src={logo} alt="logo" />} />
+    <AppBar
+      title={
+        <div>
+          <div style={{ fontSize: 32, marginTop: 10 }}>Material Data Bank</div>
+          <div style={{ fontSize: 14, fontWeight: 300 }}>An Information Portal for 3D atomic electron tomography data</div>
+        </div>
+      }
+      titleStyle={titleStyle}
+      style={style}
+      iconElementLeft={<img className='mdb-logo' src={logo} alt="logo" />} />
   );
   }
 }
--- client/src/components/main/index.js
+++ client/src/components/main/index.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
+import SearchBar from 'material-ui-search-bar'
 
 import './index.css'
 import SearchResults from '../searchresults'
 
 export default class Main extends Component {
   render = () => {
+
+    const searchBarStyle = {
+      margin: '20px auto',
+      maxWidth: 800
+    };
+
     return (
       <div>
+        <SearchBar
+          hintText={'Search by author, paper, microscope, atomic species'}
+          onChange={() => console.log('onChange')}
+          onRequestSearch={() => console.log('onRequestSearch')}
+          style={searchBarStyle}
+        />
         <SearchResults/>
       </div>
     );
