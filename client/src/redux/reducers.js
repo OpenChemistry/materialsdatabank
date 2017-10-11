f5c3d2caa6391889ee6fac75b7a77108fc69d5a7
Chris Harris <chris.harris@kitware.com>
2017-10-11 08:51:07 -0400

    Add redux-form for managing form state

--- client/src/redux/reducers.js
+++ client/src/redux/reducers.js
@@ -3,10 +3,12 @@ import { routerReducer } from 'react-router-redux'
 import tomos  from './ducks/tomos';
 import structures  from './ducks/structures';
 import reconstructions  from './ducks/reconstructions';
+import { reducer as formReducer } from 'redux-form'
 
 export default combineReducers({
   router: routerReducer,
   tomos,
   structures,
-  reconstructions
+  reconstructions,
+  form: formReducer
 });
