ad3ebff73ca6711b7254fca61c05b1dbe343ad63
Chris Harris <chris.harris@kitware.com>
2017-10-23 10:36:09 -0400

    Material => Materials

--- client/src/components/header/index.js
+++ client/src/components/header/index.js
@@ -117,7 +117,7 @@ class Header extends Component {
       <AppBar
         title={
           <div>
-            <div style={{ fontSize: 32, marginTop: 10 }}>Material Data Bank</div>
+            <div style={{ fontSize: 32, marginTop: 10 }}>Materials Data Bank</div>
             <div style={{ fontSize: 14, fontWeight: 300 }}>An Information Portal for 3D atomic electron tomography data</div>
           </div>
         }
