7972681f7bdb64af39a003d8a4e08067f5e621b4
Chris Harris <chris.harris@kitware.com>
2018-08-17 13:49:16 -0400

    Remove trailing whitespace

--- mdb/r1/Calc_R1_facts_sepFA_oldFePt_Main1.py
+++ mdb/r1/Calc_R1_facts_sepFA_oldFePt_Main1.py
@@ -9,18 +9,18 @@ Created on Fri Aug  3 02:55:49 2018
 # for R1 factor calculation, we need to input the following parameters:
     # Experimentally measured projections (currProjs:   MxMxP array, M: size of projection, P: number of projections)
     # Experimentally measured projection angles (currAngles:    Px3 array, P: number of projections)
-    # Determined atomic coordinates (currPos:   3xN array, N: number of atoms)   
+    # Determined atomic coordinates (currPos:   3xN array, N: number of atoms)
     # Determined atomic species (currAtom: 1xN array,   N: number of atoms), each element should be an integer ranging from 0 to # of species (L).
     #                           if there are total 3 different chemical species in the model, the each element of this array can be 0, 1, or 2.
-    
+
     # Determined atomic numbers for each atomic species (AtomicNumbers: 1xL array,    L: number of different kind of chemical species in the model)
-    # Determined B factors for each atomic species (BF_Array: 1xL array,    L: number of different kind of chemical species in the model)                                                
-    # Determined H factors for each atomic species (HTFact_Array: 1xL array,    L: number of different kind of chemical species in the model)    
+    # Determined B factors for each atomic species (BF_Array: 1xL array,    L: number of different kind of chemical species in the model)
+    # Determined H factors for each atomic species (HTFact_Array: 1xL array,    L: number of different kind of chemical species in the model)
     # Pixel size (resolution)
     # CropHalfWidth (# of pixels for half width, this determines how many pixels will be used for simulating each atom)
-    # VolSize (size of the projection to be simulated)         
-    # AxisConvention (xyz axis convention for GENFIRE reconstruction)   
-    # zDir (projection directon during the GENIFRE itertaion, should 1 (y-direction) or 2 (z-direction))                                 
+    # VolSize (size of the projection to be simulated)
+    # AxisConvention (xyz axis convention for GENFIRE reconstruction)
+    # zDir (projection directon during the GENIFRE itertaion, should 1 (y-direction) or 2 (z-direction))
 
 import numpy as np
 import scipy as sp
@@ -28,7 +28,7 @@ import calc_R1_function_python_GEN
 
 # load data
 stringList = 'OldFePt'
-currPos = sp.io.loadmat('currModel_{0}.mat'.format(stringList)) 
+currPos = sp.io.loadmat('currModel_{0}.mat'.format(stringList))
 currAtom = sp.io.loadmat('currAtom_{0}.mat'.format(stringList))
 currProjs = sp.io.loadmat('currProjection_{0}.mat'.format(stringList))
 currAngles =sp.io.loadmat('currAngle_{0}.mat'.format(stringList))
@@ -36,11 +36,11 @@ currAngles =sp.io.loadmat('currAngle_{0}.mat'.format(stringList))
 currPos = currPos['currModel']
 currAtom = currAtom['currAtom'] - 1
 currProjs = currProjs['currProjection']
-currAngles =currAngles['currAngle']     
+currAngles =currAngles['currAngle']
 
 # atomic numbers
 AtomicNumbers = np.array([26, 78])
-    
+
 # pixel size (resolution)
 Resolution = 0.3725
 
@@ -71,14 +71,14 @@ Result = calc_R1_function_python_GEN.calc_R1_function_indivFA_python(currPos,cur
 # print result
 calcProjs=Result[0]
 print(Result[1])
-   
+
 
 
 #%%
 '''
 # load data
 stringList = 'Particle1_meas1'
-currPos = sp.io.loadmat('currModel_{0}.mat'.format(stringList)) 
+currPos = sp.io.loadmat('currModel_{0}.mat'.format(stringList))
 currAtom = sp.io.loadmat('currAtom_{0}.mat'.format(stringList))
 currProjs = sp.io.loadmat('currProjection_{0}.mat'.format(stringList))
 currAngles =sp.io.loadmat('currAngle_{0}.mat'.format(stringList))
@@ -86,11 +86,11 @@ currAngles =sp.io.loadmat('currAngle_{0}.mat'.format(stringList))
 currPos = currPos['currModel']
 currAtom = currAtom['currAtom'] - 1
 currProjs = currProjs['currProjection']
-currAngles =currAngles['currAngle']     
+currAngles =currAngles['currAngle']
 
 # atomic numbers
 AtomicNumbers = np.array([26, 78])
-    
+
 # pixel size (resolution)
 Resolution = 0.338
 
