e60c009b142f8bfff4cb2e43a42121826dbd327e
Alessandro Genova <alessandro.genova@kitware.com>
2018-08-27 14:07:13 -0400

    Add parameters needed for the r1 calculation to the reconstruction model

--- server/materialsdatabank/server/models/reconstruction.py
+++ server/materialsdatabank/server/models/reconstruction.py
@@ -11,13 +11,21 @@ class Reconstruction(BaseAccessControlledModel):
     def validate(self, reconstruction):
         return reconstruction
 
-    def create(self, dataset, emd_file_id, user=None, public=False):
+    def create(self, dataset, emd_file_id, resolution, crop_half_width,
+                volume_size, z_direction, b_factor, h_factor,
+                axis_convention, user=None, public=False):
         reconstruction = {
             'datasetId': dataset['_id'],
-            'emdFileId': emd_file_id
+            'emdFileId': emd_file_id,
+            'resolution': resolution,
+            'crop_half_width': crop_half_width,
+            'volume_size': volume_size,
+            'z_direction': z_direction,
+            'b_factor': b_factor,
+            'h_factor': h_factor,
+            'axis_convention': axis_convention
         }
-
-
+        
         self.setPublic(reconstruction, public)
         curator = list(Group().find({
             'name': 'curator',
