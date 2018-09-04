fc9283a8dd552fb35fa7302b4b16368ec118e3b5
Chris Harris <chris.harris@kitware.com>
2018-09-04 10:16:08 -0400

    Prevent avogadro from perceiving bonds

--- server/materialsdatabank/server/avogadro.py
+++ server/materialsdatabank/server/avogadro.py
@@ -1,9 +1,12 @@
 from avogadro2 import *
+import json
 
 
 def convert_str(str_data, in_format, out_format):
     mol = Molecule()
     conv = FileFormatManager()
-    conv.readString(mol, str_data, in_format)
+    opts = {}
+    opts['perceiveBonds'] = False
+    conv.readString(mol, str_data, in_format, json.dumps(opts))
 
     return conv.writeString(mol, out_format)
