5551dcbe5b0b1d2dfe916cdcfdec4cb104a971c6
Alessandro Genova <alessandro.genova@kitware.com>
2018-11-21 12:09:39 -0500

    Update software page

--- client/src/components/software/index.js
+++ client/src/components/software/index.js
@@ -52,6 +52,38 @@ class SoftwareComponent extends Component {
 
   render() {
     const { classes } = this.props;
+    const programs = [
+      {
+        'name': 'Forward Projection',
+        'description': 'Projections from 3D reconstructions',
+        'url': 'http://www.physics.ucla.edu/research/imaging/FePt/index.html',
+        'image': ForwardImg
+      },
+      {
+        'name': 'GENFIRE',
+        'description': 'Projections to 3D reconstructions',
+        'url': 'http://genfire-em.com/',
+        'image': GenfireImg
+      },
+      {
+        'name': 'Atom Tracing',
+        'description': 'Atomic coordinates from reconstruction',
+        'url': 'http://www.physics.ucla.edu/research/imaging/FePt/index.html',
+        'image': TracingImg
+      },
+      {
+        'name': 'Tomviz',
+        'description': 'Visualization and reconstruction',
+        'url': 'https://tomviz.org',
+        'image': TomvizImg
+      },
+      {
+        'name': 'VESTA',
+        'description': 'Atomic models visualization',
+        'url': 'http://www.jp-minerals.org/vesta/en/',
+        'image': VestaImg
+      }
+    ]
     return (
       <div>
         <PageHead>
@@ -61,11 +93,11 @@ class SoftwareComponent extends Component {
           <Typography variant="subheading" paragraph color="inherit">
           </Typography>
           <Typography color="inherit" gutterBottom variant="body2">
-            Below is a non-comprehensive list of softwares and tools that can
-            visualize the atomic structures downloaded from MDB, or perform reconstructions.
+            Below is a non-comprehensive list of software and tools that can
+            perform 3D reconstructions or visualize the 3D atomic structures downloaded from MDB.
             Certain softwares (e.g. tomviz) can be used for both the visualization
-            as well as the reconstruction process.
-            The users are highly recommended to read the reference AET papers before
+            and reconstruction.
+            The users are recommended to read the reference AET papers before
             proceeding to use the software.
             For more information please refer to the links below or to the tutorials section.
           </Typography>
@@ -83,85 +115,25 @@ class SoftwareComponent extends Component {
         </PageHead>
         <PageBody>
           <Grid container style={{height: '100%'}} alignItems="stretch">
-            <Grid item xs={12} md={4} className={classes.columns}>
-              <Card>
-                <CardActionArea className={classes.cardActionArea} href="https://tomviz.org" target="_blank">
-                  <div className={classes.cardImage}>
-                    {Image(TomvizImg)}
-                  </div>
-                  <CardContent>
-                    <Typography>
-                      Tomviz
-                    </Typography>
-                    <Typography color="textSecondary">
-                      Visualization and reconstruction
-                    </Typography>
-                  </CardContent>
-                </CardActionArea>
-              </Card>
-            </Grid>
-            <Grid item xs={12} md={4} className={classes.columns}>
-              <Card>
-                <div className={classes.cardImage}>
-                  {Image(ForwardImg)}
-                </div>
-                <CardContent>
-                  <Typography>
-                    Forward Projection
-                  </Typography>
-                  <Typography color="textSecondary">
-                    Projections from 3D reconstructions
-                  </Typography>
-                </CardContent>
-              </Card>
-            </Grid>
-            <Grid item xs={12} md={4} className={classes.columns}>
-              <Card>
-                <div className={classes.cardImage}>
-                  {Image(GenfireImg)}
-                </div>
-                <CardContent>
-                  <Typography>
-                    GENFIRE
-                  </Typography>
-                  <Typography color="textSecondary">
-                    Projections to 3D reconstructions
-                  </Typography>
-                </CardContent>
-              </Card>
-            </Grid>
-            <Grid item xs={12} md={4} className={classes.columns}>
-              <Card>
-                <CardActionArea className={classes.cardActionArea} href="http://www.jp-minerals.org/vesta/en/" target="_blank">
-                  <div className={classes.cardImage}>
-                    {Image(VestaImg)}
-                  </div>
-                  <CardContent>
-                    <Typography>
-                      VESTA
-                    </Typography>
-                    <Typography color="textSecondary">
-                      Atomic models visualization
-                    </Typography>
-                  </CardContent>
-                </CardActionArea>
-              </Card>
-            </Grid>
-            <Grid item xs={12} md={4} className={classes.columns}>
-              <Card>
-                <div className={classes.cardImage}>
-                  {Image(TracingImg)}
-                </div>
-                <CardContent>
-                  <Typography>
-                    Atom Tracing
-                  </Typography>
-                  <Typography color="textSecondary">
-                    Atomic coordinates from reconstruction
-                  </Typography>
-                </CardContent>
-              </Card>
-            </Grid>
+            {programs.map(program => (
+              <Grid item key={program.name} xs={12} md={4} className={classes.columns}>
+                <Card>
+                  <CardActionArea className={classes.cardActionArea} href={program.url} target="_blank">
+                    <div className={classes.cardImage}>
+                      {Image(program.image)}
+                    </div>
+                    <CardContent>
+                      <Typography>
+                        {program.name}
+                      </Typography>
+                      <Typography color="textSecondary" noWrap>
+                        {program.description}
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </Card>
+              </Grid>
+            ))}
           </Grid>
         </PageBody>
       </div>
