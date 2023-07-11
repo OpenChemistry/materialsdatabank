242ca4b7174cc3832d59765b85351a4bafa03abb
Brady <889884m@gmail.com>
2023-07-11 10:14:32 -0700

    updated deps and JS syntax changes for updated deps

--- client/src/components/deposit/json.js
+++ client/src/components/deposit/json.js
@@ -4,7 +4,7 @@ import Button from '@material-ui/core/Button';
 
 import TextField from '@material-ui/core/TextField';
 
-import filesize from 'filesize';
+import { filesize } from 'filesize';
 
 const style = (theme) => (
   {
